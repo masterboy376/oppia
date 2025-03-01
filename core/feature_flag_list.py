9351bd7d169c3ae6e30430d82af81869f3626f39
Sambhav Kaushik <masterboy376@gmail.com>
2025-03-01 20:57:57 +0530

    added feature flag

--- core/feature_flag_list.py
+++ core/feature_flag_list.py
@@ -46,6 +46,7 @@ class FeatureNames(enum.Enum):
     SHOW_FEEDBACK_UPDATES_IN_PROFILE_PIC_DROPDOWN = (
         'show_feedback_updates_in_profile_pic_dropdown')
     CD_ADMIN_DASHBOARD_NEW_UI = 'cd_admin_dashboard_new_ui'
+    SHOW_TRANSLATION_SUGGESTIONS_IN_CD = 'show_translation_suggestions_in_cd'
     IS_IMPROVEMENTS_TAB_ENABLED = 'is_improvements_tab_enabled'
     LEARNER_GROUPS_ARE_ENABLED = 'learner_groups_are_enabled'
     NEW_LESSON_PLAYER = 'new_lesson_player'
@@ -87,7 +88,8 @@ DEV_FEATURES_LIST = [
     FeatureNames.SHOW_TRANSLATION_SIZE,
     FeatureNames.NEW_LESSON_PLAYER,
     FeatureNames.REDESIGNED_TOPIC_VIEWER_PAGE,
-    FeatureNames.AUTOMATIC_VOICEOVER_REGENERATION_FROM_EXP
+    FeatureNames.AUTOMATIC_VOICEOVER_REGENERATION_FROM_EXP,
+    FeatureNames.SHOW_TRANSLATION_SUGGESTIONS_IN_CD
 ]
 
 # Names of features in test stage, the corresponding feature flag instances must
@@ -190,6 +192,13 @@ FEATURE_FLAG_NAME_TO_DESCRIPTION_AND_FEATURE_STAGE = {
         (
             'This flag is to show new contributor admin dashboard.',
             feature_flag_domain.ServerMode.TEST
+        )
+    ),
+    FeatureNames.SHOW_TRANSLATION_SUGGESTIONS_IN_CD.value: (
+        (
+            'This flag is to show auto-generated translation suggestions in ',
+            'the contributor dashboard.',
+            feature_flag_domain.ServerMode.DEV
         )
     ),
     FeatureNames.IS_IMPROVEMENTS_TAB_ENABLED.value: (
